cde0540fb90c9fe424bdb2258e830b4ca43d84d0
Gal Jeza <gal.jeza55@gmail.com>
2025-04-28 09:51:04 +0200

    first

---
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,15 @@
+import { SignUp } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <SignUp 
+        appearance={{
+          elements: {
+            formButtonPrimary: "bg-slate-500 hover:bg-slate-600",
+          },
+        }}
+      />
+    </div>
+  );
+} 
